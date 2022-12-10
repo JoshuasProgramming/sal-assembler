5338bd68e562e26216d88b213837edd866826830
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2022-12-10 22:04:43 +0000

    continued development on sal_assembler

--- sal_assembler.java
+++ sal_assembler.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
+import java.util.regex.Pattern;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.util.Arrays;
@@ -12,7 +14,23 @@ public class sal_assembler{
     // static ArrayList<String> salEncodedArray = new ArrayList<String>();
     static String salEncodedArray[];
 
+    static ArrayList<String> outputTxtItems = new ArrayList<String>();
+
+    static String salName = "";
+
+    // static FileWriter outputTextFile = new FileWriter("output.txt");
+   
     public static void main(String[] args) throws FileNotFoundException {
+
+        // try{
+        //     FileWriter outputTextFile  = new FileWriter("C:/Users/joshu/Documents/output.txt");
+            
+        // } catch (IOException e) {
+        //     // TODO Auto-generated catch block
+        //     e.printStackTrace();
+        // }
+
+
         boolean isSal = false;
 
         Scanner scanner = new Scanner(System.in);
@@ -32,26 +50,19 @@ public class sal_assembler{
             }
             
         }
+
+        // System.out.println(outputTxtItems);
     }
 
     public static void readFile(File salFile){
         
         try (BufferedReader reader = new BufferedReader(new FileReader(salFile))) {
             String line = reader.readLine();
-            int lineNumber = 0;
-
-            String[] lines;
-
 
             while(line != null){
-                // System.out.println(line);
                 encode(line);
                 line = reader.readLine();
-
-                lineNumber++;
-                
             }
-
             encode(line);
 
             reader.close();
@@ -62,82 +73,240 @@ public class sal_assembler{
     }
 
     public static void encode(String line){
+
+        String rva = null;
+        String rva01 = null;
+        final ArrayList<String> rva_arr = new ArrayList<String>();
+        final ArrayList<String> size_arr = new ArrayList<String>();
+        
+
+
+        try (FileWriter outputTextFile = new FileWriter("output.txt")) {
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         
         //declare an empty String array
         String[] arr = null;
 
+        String arr2 = null;
+
         //convert the string
-        arr = line.replaceAll("\\s", "\n").split("\\s+");
+        if(line != null){
+            arr = line.replaceAll("\\s", "\n").split("\\s+");
+        }
 
-        for(int i = 0; i < arr.length; i++){
+        arr2 = line;
+        
+        boolean rvaOn = false;
 
-            if(arr[i].isEmpty()){
-               i++;
-            }  
+        if(arr != null){
 
-            switch(arr[i]){
-                case "LOAD": 
-                if(arr[i] == "#"){
-                    String rva = "01";
+            for(int i = 0; i < arr.length; i++){
+
+                if(arr[i].isEmpty()){
+                   i++;
+                }  
+
+                // System.out.println(arr2.replaceAll("\\s+", "\n"));
+
+                if(arr2.replaceAll("\\s+", "\n").contains("#")){
+                    rva_arr.add("01");
+                } else {
+                    rva = "00";
                 }
-                output("1100");
-                break;
 
-                case "SUB": 
-                output("0001");
-                break;
+                Matcher m = Pattern.compile("[-+]?\\d+").matcher(arr2.replaceAll("\\s+", "\n"));
 
-                case "OR": 
-                output("0011");
-                break;
-
-                case "JMP": 
-                output("0100");
-                break;
-
-                case "JGT": 
+                if(m.find()){
+                    int number = Integer.parseInt(m.group());
+                    if(number > 63){
+                        size_arr.add("1");
+                    } else {
+                        size_arr.add("0");
+                    }
+                }
                 
-                output("0101");
-                break;
+                System.out.println(size_arr);
 
-                case "JLT": 
-                output("0110");
-                break;
+                // char[] ch = new char[arr[i].length()];
 
-                case "JEQ": 
-                output("0111");
-                break;
+                // for(int k = 0; k < arr[i].length(); k++){
+                //     ch[k] = arr[i].charAt(k);
 
-                case "unused": 
-                output("1000");
-                break;
+                //     if(ch[k] == '#'){
+                //         rva01 = "01";
+                //         System.out.println(ch[k] + " " + 1);
+                //         // System.out.println(rva01);
+                //         rva_arr.add("01");
+                //         System.out.println(rva_arr + " " + 1);
+                //         rvaOn = true;
 
-                case "INC": 
-                output("1001");
-                break;
+                //     } else {
+                //         rva = "00";
+                //         // System.out.println(rva);
+                //         System.out.println(ch[k]);
+                        
+                //     }
+                // }
 
-                case "DEC": 
-                output("1010");
-                break;
+                // System.out.println(rva_arr.contains("01") + " " + rva_arr + " " + rvaOn);
+                
 
-                case "NOT": 
-                output("1011");
-                break;
+    
+                switch(arr[i]){
 
-                case "STORE": 
-                output("1101");
-                break;
+                    case "LOAD": 
 
-                case "ADD":
-                output("0000");
-                break;
-            }
+                    if(rva_arr.contains("01")){
+                        output("1100", "01");
+                    } else {
+                        output("1100", rva);
+                    }
+
+                    break;
+    
+                    case "SUB": 
+
+                    if(rva_arr.contains("01") ){
+                        output("0001", "01");
+                    } else {
+                    output("0001", rva);
+                    }
+                    break;
+    
+                    case "OR": 
+
+                    if(rva_arr.contains("01") ){
+                        output("0011", "01");
+                    } else {
+                    output("0011", rva);
+                    }
+                    break;
+    
+                    case "JMP": 
+
+                    if(rva_arr.contains("01") ){
+                        output("0100", "01");
+                    } else {
+                    output("0100", rva);
+                    }
+                    break;
+    
+                    case "JGT": 
+
+                    if(rva_arr.contains("01") ){
+                        output("0101", "01");
+                    } else {
+                    output("0101", rva);
+                    }
+                    break;
+    
+                    case "JLT": 
+                    if(rva_arr.contains("01") ){
+                        output("0110", "01");
+                    } else {
+                    output("0110", rva);
+                    }
+                    break;
+    
+                    case "JEQ": 
+                    if(rva_arr.contains("01") ){
+                        output("0111", "01");
+                    } else {
+                    output("0111", rva);
+                    }
+                    break;
+    
+                    case "unused": 
+                    if(rva_arr.contains("01") ){
+                        output("1000", "01");
+                    } else {
+                    output("1000", rva);
+                    }
+                    break;
+    
+                    case "INC": 
+                    if(rva_arr.contains("01") ){
+                        output("1001", "01");
+                    } else {
+                    output("1001", rva);
+                    }
+                    break;
+    
+                    case "DEC": 
+                    if(rva_arr.contains("01") ){
+                        output("1010", "01");
+                    } else {
+                    output("1010", rva);
+                    }
+                    break;
+    
+                    case "NOT": 
+                    if(rva_arr.contains("01") ){
+                        output("1011", "01");
+                    } else {
+                    output("1011", rva);
+                    }
+                    break;
+    
+                    case "STORE": 
+                    if(rva_arr.contains("01") ){
+                        output("1101", "01");
+                    } else {
+                    output("1101", rva);
+                    }
+                    break;
+    
+                    case "ADD":
+                    if(rva_arr.contains("01") ){
+                        output("0000", "01");
+                    } else{
+                        output("0000", rva);
+                    }
+                    
+                    break;
+                }
+
+        }
             
         }
+
+        // outputTextFile(line, line, line, line, line);
+
+        
         // C:/Users/joshu/Downloads/sal-examples/ex1.sal
     }
+    
 
-    public static void output(String instruction){
-        System.out.println(instruction + "000" + "0" + "00" + "000000");
+    public static void output(String instruction, String rva){
+
+        //print the binary translation
+        System.out.println(instruction + "000" + "0" + rva + "000000");
+
+        
+
+        //insert each sub section of the binary instruction to the 'outputTextFile' function as parameter
+        // outputTextFile(instruction, "000", "0", "00", "000000");
+        outputTextFile(instruction, "00", "0", rva, "000000");
+        outputTxtItems.add(instruction + "000" + "0" + rva + "000000");
+        
+    }
+
+    public static void outputTextFile(String instruction, String r, String size, String rva, String operand){
+        try{
+            FileWriter outputTextFile  = new FileWriter("C:/Users/joshu/Documents/" + "ex1" + ".bin");
+
+            for(int i = 0; i < outputTxtItems.size(); i++){
+                outputTextFile.write(instruction + "000" + "0" + rva + "000000" + "\n");
+            }
+           
+            outputTextFile.close();
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }        
     }
 }
